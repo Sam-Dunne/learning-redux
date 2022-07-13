ea6a4f23026fd4c0df5cc877fd32ca4144caf44e
Sam-Dunne <cras.sdunne@gmail.com>
2022-07-13 13:33:36 -0500

    creating SCSS variables for BS override, adding 'count' state to Navbar

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -10,7 +10,9 @@ function Home() {
         <main className="container">
             <section className="">
                 <div className="">
-                    <h1 className="text-center">RTK/React Router V6 Playground</h1>
+                    <h1 className="text-center">ReduxToolkit, React-Router V6, SASS Playground</h1>
+                    <p className='paragraph-wrapper'>The purpose of this project is to demonstrate basic understanding and application of the above technologies. With a focus of increasing the scalabilty and managability of applications.</p>
+                    <p className="paragraph-wrapper"></p>
                 </div>
                 <CounterCard />
             </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 export default function Navbar() {
+  const { count } = useSelector((state) => state.counter);
+
     return (
         <nav className='navbar bg-primary w-full p-3' >
 
             {/* <div className=' p-3'> */}
                 {/* <div className=''> */}
                     <Link to='/' className='btn btn-light text-primary'>Rtk Playground</Link>
+                    <div className='counter-value-wrapper'>
+                        <div className='text-primary'>{count}</div>
+                    </div>
                     <Link to='/counter' className='btn btn-light text-primary'>Counter</Link>
                 {/* </div> */}
 
